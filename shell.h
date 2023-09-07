86636ae6abe2ee03597f59c8a97c11eca14b547a
Muna <munasaeed6367@gmail.com>
2023-09-06 17:04:38 -0700

    added a header file that contains function prototypes

--- shell.h
+++ shell.h
@@ -4,5 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
 void moon_print(const char *printf_msg);
+void render_prompt(void);
+void exec_prompt(const char *directive);
+
 #endif
