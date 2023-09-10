5ffada8355e91f5c9d7c2804bfc785bd567527b5
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-09-10 23:45:35 +0000

    added moon_getline.c file and solved some errors

--- my_shell_main.c
+++ my_shell_main.c
@@ -1,20 +1,25 @@
 #include "shell.h"
 /**
  * main - Entry point.
- * Description: this function is where the execution of a C program starts.
+ * Description: This function is where the execution of a C program starts.
  * @return The exit status of the program.
  * A value of 0 indicates successful execution.
-*/
+ */
+/* Define the global variables */
+ssize_t input_index = 0;
+char input_buffer[MAX_INPUT_SIZE];
+
 int main(void)
 {
 	char user_input[150];
 
-	while (true)
+	while (1) // Use 1 for an infinite loop
 	{
 		render_prompt();
 		read_user_input(user_input, sizeof(user_input));
 		exec_prompt(user_input);
-		
-		}
-	return (0);
+	}
+
+	return 0;
 }
+
--- shell.h
+++ shell.h
@@ -7,13 +7,25 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include<stdbool.h>
+#include <stdbool.h>
 
 #define MAX_PATH_LENGTH 256
+#define MAX_INPUT_SIZE 1024
 
+/* Global variables */
+extern ssize_t input_index;             // Declaration of input_index as a global variable
+extern char input_buffer[MAX_INPUT_SIZE]; // Declaration of input_buffer as a global variable
+extern char **environ;
+
+/* Function prototypes */
 void moon_print(const char *printf_msg, ...);
 void render_prompt(void);
 void exec_prompt(const char *user_input);
 void read_user_input(char *user_input, size_t size);
+ssize_t moon_getline(char **lineptr, size_t *n);
+void tokenize_input(const char *user_input, char *arguments[], int *arguments_counter);
+void handle_builtin_commands(char *arguments[], int arguments_counter);
+void handle_other_commands(char *arguments[]);
 
-#endif
+#endif /* SHELL_H */
+
